a5b535bc8a985ecc78d8db9b1566a3f82fd45703
Pavel <nemkovp@list.ru>
2025-08-27 11:59:56 +0300

    start

--- src/main/java/com/springpro/todoapp/model/TodoItem.java
+++ src/main/java/com/springpro/todoapp/model/TodoItem.java
@@ -1,11 +1,33 @@
 package com.springpro.todoapp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class TodoItem {
 
-    Long id;
-    String title;
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
 
+    public TodoItem() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
